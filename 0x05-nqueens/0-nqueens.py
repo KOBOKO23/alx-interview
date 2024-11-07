98441c9765fc9b6a48ec5374e40bc0f6c601cc97
KOBOKO23 <kobokomasero@gmail.com>
2024-11-07 16:46:23 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,42 +4,28 @@ import sys
 
 
 def print_board(board, n):
-    """prints allocated possitions to the queen"""
-    b = []
-
-    for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                b.append([i, j])
-    print(b)
+    """Prints allocated positions of the queens."""
+    solution = [[i, board[i]] for i in range(n)]
+    print(solution)
 
 
-def safe_position(board, i, j, r):
-    """Determines whether the position is safe for the queen"""
-    if (board[i] == j) or (board[i] == j - i + r) or (board[i] == i - r + j):
-        return True
-    return False
+def safe_position(board, row, col):
+    """Checks if the position is safe for a queen."""
+    for i in range(row):
+        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+            return False
+    return True
 
 
 def determine_positions(board, row, n):
-    """Recursively finds all safe positions where the queen can be allocated"""
+    """Recursively finds all safe positions for queens."""
     if row == n:
         print_board(board, n)
-
     else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if safe_position(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
+        for col in range(n):
+            if safe_position(board, row, col):
+                board[row] = col
                 determine_positions(board, row + 1, n)
-
-
-def create_board(size):
-    """Generates the board"""
-    return [0 * size for i in range(size)]
 
 
 if len(sys.argv) != 2:
@@ -48,14 +34,14 @@ if len(sys.argv) != 2:
 
 try:
     n = int(sys.argv[1])
-except BaseException:
+except ValueError:
     print("N must be a number")
     exit(1)
 
-if (n < 4):
+if n < 4:
     print("N must be at least 4")
     exit(1)
 
-board = create_board(int(n))
-row = 0
-determine_positions(board, row, int(n))
+board = [-1] * n
+determine_positions(board, 0, n)
+
